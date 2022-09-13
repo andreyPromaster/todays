9599ed6899f9f70980c869ba4c4da43008171fda
Aliaksei Valenta <liverpoolsmile23@gmail.com>
2022-09-13 22:43:07 +0400

    create tests for get theme and create new theme

--- app/tests/api/test_theme.py
+++ app/tests/api/test_theme.py
@@ -1,8 +1,28 @@
+import pytest
+from db.models.news import Theme
 from tests.factories import ThemeFactory
 from tests.utils import client
 
 
 def test_get_theme(db_session):
-    theme = ThemeFactory()
-    response = client.get(f"/theme/{theme.id}")
-    assert response.status_code == 201
+    theme = ThemeFactory(db_session).create()
+    response = client.get(f"/themes/{theme.id}")
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize(
+    "data,expected_value",
+    [
+        ({"name": "New Theme"}, None),
+        ({"name": "Any new Theme", "description": "some description"}, "some description"),
+    ],
+)
+def test_create_new_theme(db_session, data, expected_value):
+    theme = data
+    response = client.post(
+        "/themes/", json={"name": theme["name"], "description": theme.get("description")}
+    )
+    expected_theme = db_session.query(Theme).first()
+    assert response.status_code == 200
+    assert expected_theme.name == theme["name"]
+    assert expected_theme.description == expected_value
--- app/tests/factories.py
+++ app/tests/factories.py
@@ -1,7 +1,6 @@
 import factory
 from db.models.news import Theme, User
 from sqlalchemy.orm import Session
-from tests.utils import TestingSessionLocal
 
 
 def UserFactory(session: Session):
@@ -17,11 +16,14 @@ def UserFactory(session: Session):
     return _UserFactory
 
 
-class ThemeFactory(factory.alchemy.SQLAlchemyModelFactory):
-    class Meta:
-        model = Theme
-        sqlalchemy_session_persistence = "commit"
-        sqlalchemy_session = TestingSessionLocal()
+def ThemeFactory(session: Session):
+    class _ThemeFactory(factory.alchemy.SQLAlchemyModelFactory):
+        class Meta:
+            model = Theme
+            sqlalchemy_session_persistence = "commit"
+            sqlalchemy_session = session
 
-    name = factory.Sequence(lambda n: f"Theme-{n}")
-    description = "Theme description"
+        name = factory.Sequence(lambda n: f"Theme-{n}")
+        description = "Theme description"
+
+    return _ThemeFactory

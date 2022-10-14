f3149885224088de0e2cba8ff7ba7c09c0eb7024
andreyPromaster <and45.rew@ya.ru>
2022-10-14 15:25:56 +0300

    Make original link text, optimize NewsApiManager

--- app/parsers/services.py
+++ app/parsers/services.py
@@ -23,45 +23,66 @@ class ArticleManager(ABC):
 
     @abstractmethod
     def sync_news(self):
-        """Use url news like unique key. The class is responsible for
-        applying filters, process already saved news, update changed news"""
+        """
+        Use url news like unique key. The class is responsible for
+        applying filters, process already saved news, update changed news,
+        control that the newest news will be uploaded.
+        """
 
 
 class NewsApiManager(ArticleManager):
     api_cls = NewsApi
 
     def sync_news(self):
-        # create model for set up Api provided with data with access to news
-        # model source shows only possible newsmaker portal
-        # configurate filters
+        pass
+
+    def upload_chunk_of_news(self):
+        """
+        Create model for set up Api provided with data with access to news
+        model source shows only possible newsmaker portal
+        configurate filters
+        """
         settings = CoreSettings()
         api = self.create_api(
             base_url="https://newsapi.org/v2/top-headlines", creds={"api_key": settings.API_KEY}
         )
 
         news = api.get_news()
-        news_emails = {item.original_link: item for item in news}
+        news_links = {item.original_link: item for item in news}
 
         uploaded_news = (
             self.db_session.query(News.uploaded_at, News.original_link)
-            .filter(News.original_link.in_(news_emails.keys()))
+            .filter(News.original_link.in_(news_links.keys()))
             .all()
         )
+        # queries = [
+        #     self.db_session.query(News.uploaded_at, News.original_link).filter(
+        #         News.original_link == link
+        #     )
+        #     for link in news_links.keys()
+        # ]
+        # uploaded_news = (
+        #     self.db_session.query(News.uploaded_at, News.original_link)
+        #     .from_statement(union(*queries))
+        #     .all()
+        # )
+
         length_prepared_news = len(news)
 
         for uploaded_at, link in uploaded_news:
-            if news_emails.get(link) is None:
+            if news_links.get(link) is None:
                 continue
-            del news_emails[link]
+            del news_links[link]
 
-        length_uploaded_news = len(news_emails.keys())
+        length_uploaded_news = len(news_links.keys())
 
-        self.db_session.add_all((News(**item.dict()) for item in news_emails.values()))
+        self.db_session.bulk_insert_mappings(News, (item.dict() for item in news_links.values()))
         self.db_session.commit()
         return (
             f"{length_prepared_news - length_uploaded_news}/"
             f"{length_prepared_news} news have been uploaded."
         )
+
         # second approach
         # for item in news:
         #     try:
@@ -71,7 +92,3 @@ class NewsApiManager(ArticleManager):
         #         print(e)
         # to do: connect news to source. Manage amount uploaded
         #  news, check last uploaded news, then stop uploading
-
-
-a = NewsApiManager()
-a.sync_news()
